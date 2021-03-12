90f9731291c71bcb75aa2db55d0de48614749328
Sinkvane <sinkvane@gmail.com>
2021-03-12 19:38:37 +0600

    better-nav

--- burger.js
+++ burger.js
@@ -1,4 +1,5 @@
 let button = document.getElementById("burger");
+let closeBtn = document.querySelector('.homepage-top');
 
 button.onclick = function () {
   let body = document.getElementById("body");
@@ -8,4 +9,14 @@ button.onclick = function () {
     body.classList.toggle('mm-wrapper_background'),
     body.classList.toggle('mm-wrapper_opening');
   menu.classList.toggle('mm-menu_opened');
+};
+
+closeBtn.onclick = function () {
+  let body = document.getElementById("body");
+  let menu = document.getElementById("m-menu-main");
+  body.classList.remove('mm-wrapper_opened'),
+    body.classList.remove('mm-wrapper-wrapper_blocking'),
+    body.classList.remove('mm-wrapper_background'),
+    body.classList.remove('mm-wrapper_opening');
+  menu.classList.remove('mm-menu_opened');
 };
