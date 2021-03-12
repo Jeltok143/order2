04e8fb764ca7d842e0c2e474a35afc95cbb6138b
Sinkvane <sinkvane@gmail.com>
2021-03-12 20:39:43 +0600

    fix

--- burger.js
+++ burger.js
@@ -1,5 +1,7 @@
 let button = document.getElementById("burger");
 let closeBtn = document.querySelector('.homepage-top');
+let closeBtn2 = document.querySelector('.home__sales__section');
+let closeBtn3 = document.querySelector('.catalog-page');
 
 button.onclick = function () {
   let body = document.getElementById("body");
@@ -12,6 +14,26 @@ button.onclick = function () {
 };
 
 closeBtn.onclick = function () {
+  let body = document.getElementById("body");
+  let menu = document.getElementById("m-menu-main");
+  body.classList.remove('mm-wrapper_opened'),
+    body.classList.remove('mm-wrapper-wrapper_blocking'),
+    body.classList.remove('mm-wrapper_background'),
+    body.classList.remove('mm-wrapper_opening');
+  menu.classList.remove('mm-menu_opened');
+};
+
+closeBtn2.onclick = function () {
+  let body = document.getElementById("body");
+  let menu = document.getElementById("m-menu-main");
+  body.classList.remove('mm-wrapper_opened'),
+    body.classList.remove('mm-wrapper-wrapper_blocking'),
+    body.classList.remove('mm-wrapper_background'),
+    body.classList.remove('mm-wrapper_opening');
+  menu.classList.remove('mm-menu_opened');
+};
+
+closeBtn3.onclick = function () {
   let body = document.getElementById("body");
   let menu = document.getElementById("m-menu-main");
   body.classList.remove('mm-wrapper_opened'),
